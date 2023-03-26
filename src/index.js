fac5fd6a44e25989a26b4a6929a384790c1c0b68
jsKraken <javascriptiqbal@gmail.com>
2023-03-26 14:49:15 +0600

    Update to Functional Component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App.component';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.component";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const div = document.getElementById('root');
+const div = document.getElementById("root");
 const root = ReactDOM.createRoot(div);
 root.render(
-  <div className='header'>
-    <div className='container'>
-        <div className='row'>
-            <App />
+    <div className="header">
+        <div className="container">
+            <div className="row">
+                <App />
+            </div>
         </div>
     </div>
-  </div>
 );
